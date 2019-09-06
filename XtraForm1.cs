023761c0ddcb3b599674764561eeb139356d4819
hovanvinh <41520147+hovanvinh@users.noreply.github.com>
2019-09-06 12:51:17 +0700

    Form 1
    thiet ke giao dien ban hnang

---
+++ XtraForm1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace QL_Hoa_Hong
+{
+    public partial class XtraForm1 : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public XtraForm1()
+        {
+            InitializeComponent();
+        }
+        void navBarControl_ActiveGroupChanged(object sender, DevExpress.XtraNavBar.NavBarGroupEventArgs e)
+        {
+            navigationFrame.SelectedPageIndex = navBarControl.Groups.IndexOf(e.Group);
+        }
+        void barButtonNavigation_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            int barItemIndex = barSubItemNavigation.ItemLinks.IndexOf(e.Link);
+            navBarControl.ActiveGroup = navBarControl.Groups[barItemIndex];
+        }
+
+        private void navBarControl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employeesLabelControl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void officeNavigationBar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XtraForm1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
